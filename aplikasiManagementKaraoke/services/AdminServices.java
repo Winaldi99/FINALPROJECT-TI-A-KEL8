8cf76c9d874b3a0ee83763613800b8e07cb4ca8d
Winaldi <2381030@unai.edu>
2024-12-10 22:43:33 +0700

    Commit

--- aplikasiManagementKaraoke/services/AdminServices.java
+++ aplikasiManagementKaraoke/services/AdminServices.java
@@ -13,4 +13,7 @@ public interface AdminServices{
     void editLagu();
     void addRuangan();
     ArrayList<Lagu> getAllLagu();
+    void addDaftarLaguRuangan(int idRuangan);
+    void addLagu();
+    Lagu cariLagu(String namaLagu);
 }
